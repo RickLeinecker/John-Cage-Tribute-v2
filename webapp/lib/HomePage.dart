1567a883c85022ae94131a6b826bcfe2e830d69f
Daniel Hernandez-Otero <danielhotero96@gmail.com>
2022-08-28 22:53:45 -0400

    Admin login created to prevent unauthorized access. Leads to a blank, WIP page.

--- webapp/lib/HomePage.dart
+++ webapp/lib/HomePage.dart
@@ -10,12 +10,53 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {},
-          child: const Text('Go Back'),
-        ),
-      ),
+      body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("images/college_bg.jpg"),
+            fit: BoxFit.cover,
+          )),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                child: Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.70),
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Center(
+                        child: Text(
+                            'We\'re still a work in progress! Check back soon.',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center)))),
+            SizedBox(height: 20),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                child: Container(
+                    height: 100,
+                    width: 600,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.70),
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Center(
+                        child: Text(
+                      'Pictured: Black Mountain College, where John Cage taught many of his students in avant-garde music. Here, he organized the first \'Happening\'.',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18,
+                        letterSpacing: 2,
+                      ),
+                      textAlign: TextAlign.center,
+                    ))))
+          ])),
     );
   }
 }
