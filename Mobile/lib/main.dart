d0c66e2adba37692d404a721f2281afa5590a661
Chandler Hale <cbeckerhale@knights.ucf.edu>
2022-09-02 16:39:41 -0400

    Adding Home Page functionality

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo'),
+      home: const HomePage(title: 'HomePage'),
     );
   }
 }
