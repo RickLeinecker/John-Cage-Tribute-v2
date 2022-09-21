42c87940aab0ec639bfe46633f1affbc077a9a0c
Chandler Hale <cbeckerhale@knights.ucf.edu>
2022-09-20 23:58:58 -0400

    Redesigned Landing Page

--- Website/client/src/App.js
+++ Website/client/src/App.js
@@ -22,7 +22,6 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
-          <Navbar />
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route component={Routes} />
