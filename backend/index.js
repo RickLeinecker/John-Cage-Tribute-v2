88fcf7e912c3378f297be71d683b73e30acc28fd
Cameron Berezuk <camberezuk@gmail.com>
2022-10-19 16:32:17 -0400

    added api call to get user recordings

--- backend/index.js
+++ backend/index.js
@@ -36,14 +36,22 @@ app.get("/recordings", (req, res) => {
 });
 
 // List user's recordings
+app.get("/userRec", (req, res) => {
+    const s  = 1 // going to switch this to user id that is passed through token
+    db2.query("SELECT DISTINCT R.recordingId, R.maestroId, R.title, R.lengthSeconds, R.audioFile, R.inContest, DATE_FORMAT(R.recordingDate, '%M-%d-%Y') AS date FROM Users U LEFT JOIN UserRecording T ON '%" + s + "%' = T.userId LEFT JOIN Recordings R ON R.recordingId = T.recordingId",
+    (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+        res.send(result);
+    }
+    });
+});
 
 // Search Composition by Title, Maestro, or Date
 app.get("/title", (req, res) => {
     const s  = req.query.query;
-    console.log("Req.body: ", s)
-    console.log("Req.body.query: ", req.body.query)
-    const titlex = s;
-    console.log("TITLE:", titlex);
     db2.query("SELECT DISTINCT R.recordingId, R.maestroId, R.title, R.lengthSeconds, R.audioFile, R.inContest, DATE_FORMAT(R.recordingDate, '%M-%d-%Y') AS date, U.username FROM Recordings R, Users U WHERE R.maestroId = U.id AND ((R.title LIKE '%" + s + "%') OR (U.username LIKE '%" + s + "%') OR (R.recordingDate LIKE '%" + s + "%'))",
     (err, result) => {
     if (err) {
@@ -81,3 +89,8 @@ app.delete("/delete/:id", (req, res) => {
       }
     });
   });
+
+// Create Recording
+
+// Create UserRecording
+
