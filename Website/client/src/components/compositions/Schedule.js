bf57413229113a50eb50e4e391ebd725b0a360b5
Chandler Hale <cbeckerhale@knights.ucf.edu>
2022-10-26 13:19:36 -0400

    Adding functionality to schedule page

--- Website/client/src/components/compositions/Schedule.js
+++ Website/client/src/components/compositions/Schedule.js
@@ -5,8 +5,33 @@ import { connect } from 'react-redux';
 import './Calendar.css';
 
 const Schedule = ({ isAuthenticated }) => {
-    //TEMPORARY REPLACE WITH REAL DATA
-    const temp_times = ["00:00", "2:00", "4:00", "6:00", "8:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"];
+    const times =
+    [
+        "00:00", "00:20", "00:40",
+        "1:00", "1:20", "1:40",
+        "2:00", "2:20", "2:40",
+        "3:00", "3:20", "3:40",
+        "4:00", "4:20", "4:40",
+        "5:00", "5:20", "5:40",
+        "6:00", "6:20", "6:40",
+        "7:00", "7:20", "7:40",
+        "8:00", "8:20", "8:40",
+        "9:00", "9:20", "9:40",
+        "10:00", "10:20", "10:40",
+        "11:00", "11:20", "11:40",
+        "12:00", "12:20", "12:40",
+        "13:00", "13:20", "13:40",
+        "14:00", "14:20", "14:40",
+        "15:00", "15:20", "15:40",
+        "16:00", "16:20", "16:40",
+        "17:00", "17:20", "17:40",
+        "18:00", "18:20", "18:40",
+        "19:00", "19:20", "19:40",
+        "20:00", "20:20", "20:40",
+        "21:00", "21:20", "21:40",
+        "22:00", "22:20", "22:40",
+        "23:00", "23:20", "23:40"
+    ];
 
     const [value, onChange] = useState(new Date());
 
@@ -23,16 +48,18 @@ const Schedule = ({ isAuthenticated }) => {
                             Choose a date for your concert:
                         </h1>
                         <br/>
+                        <form onSubmit={handleSubmit}>
                         <Calendar
                             value={value}
                             onChange={onChange}
                         />
                         <div className='schedule-times'>
-                            {temp_times.map((time) =>
-                                <button className='btn-times'>{time}</button>
+                            {times.map((time) =>
+                                <button className=' btn btn-times'>{time}</button>
                             )}
                         </div>
                         <input type="submit" className="btn btn-primary" value="Reserve" />
+                        </form>
                     </div>
                 </div>
             </div>
@@ -44,5 +71,10 @@ const Schedule = ({ isAuthenticated }) => {
 const mapStateToProps = (state) => ({
     isAuthenticated: state.auth.isAuthenticated
 });
+
+const handleSubmit = event => {
+    event.preventDefault();
+    //Add handleSubmit here
+}
 
 export default connect(mapStateToProps)(Schedule);
