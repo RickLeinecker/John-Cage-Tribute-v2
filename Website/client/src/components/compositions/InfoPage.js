e6052420db02bd17295ce0749fa7301703a2810e
Chandler Hale <cbeckerhale@knights.ucf.edu>
2022-11-08 18:30:20 -0500

    Finished Info Page

--- Website/client/src/components/compositions/InfoPage.js
+++ Website/client/src/components/compositions/InfoPage.js
@@ -1,14 +1,15 @@
 import React from "react";
+import jcpic2 from "../../img/jcpic2.JPG";
 
 class InfoPage extends React.Component {
     render() {
         return(
             <div className='schedule'>
                 <div className='dark-overlay'>
-                    <div className='search-inner'>
-                        <div className='search-box'>
+                    <div className='schedule-inner'>
+                        <div className='schedule-box'>
                             <h1 className='large text-primary'>About John Cage</h1>
-                            <span>
+                            <div className='landing-bottom'>
                                 <div className='info-left'>
                                     <p className='landing-text'>
                                         John Cage was a composer who challenged the concept of music itself.
@@ -17,10 +18,15 @@ class InfoPage extends React.Component {
                                         He began composing music, and took a very mathematical approach, rather than an emotional one, to composition.
                                         Cage has made many strides in experimenting with what it means for music to “be” music.
                                         He toyed with the very definition of music until eventually coming up with his musical philosophy.
+                                        John Cage believed that there was no distinction between sound and music.
+                                        If it made waves that disturbed the air, it was considered both music and sound to him.
+                                        4’33” is John Cage's most famous piece, where the performer is instructed to not play their instrument at all.
+                                        Cage also believed in stripping away the artist’s or performer’s control over the piece.
+                                        Our site expands upon this belief and captures the 'music' around us.
                                     </p>
-                                    {/* <img src='' /> */}
                                 </div>
-                            </span>
+                                <img src={jcpic2} alt='John Cage Piano' width='485px' />
+                            </div>
                         </div>
                     </div>
                 </div>
