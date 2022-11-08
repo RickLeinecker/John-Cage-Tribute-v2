814bae71cb70048c15195a602fe222168af6a09c
Chandler Hale <cbeckerhale@knights.ucf.edu>
2022-11-07 23:47:34 -0500

    Trying to connect apis

--- Website/client/src/components/compositions/Profile.js
+++ Website/client/src/components/compositions/Profile.js
@@ -12,185 +12,148 @@ import setAuthToken from "../../utils/setAuthToken";
 class Profile extends React.Component {
     constructor(props) {
 		super(props);
-		console.log("PROPS", props.username);
+        const id = JSON.parse(localStorage.getItem("token")).userId;
 		this.state = {
 			username: "",
-			userId: props.id,
+			userId: id,
 			email: "",
             bio: "",
             isMaestro: -1,
 			editing: false,
-			// formdata: {
-			// 	title: props.info.title,
-			// 	length: props.info.lengthSeconds,
-			// 	date: props.info.recordingDate
-			// 	// tags: props.info.tags.join(","),
-			// 	// description: props.info.description,
-			// 	// private: props.info.private
-			// }
+            editName: "",
+            editBio: "",
 		}
-		//this.chosenState = this.chosenState.bind(this);
-		// this.parseDateTime = this.parseDateTime.bind(this);
-		// this.deleteComp = this.deleteComp.bind(this);
-		// this.changeForm = this.changeForm.bind(this);
-		// this.submitEdit = this.submitEdit.bind(this);
-		// this.cancelEdit = this.cancelEdit.bind(this);
-		// this.setPrivate = this.setPrivate.bind(this);
+        this.changeUsername = this.changeUsername.bind(this);
+        this.changeBio = this.changeBio.bind(this);
+        this.submitEdit = this.submitEdit.bind(this);
+        this.cancelEdit = this.cancelEdit.bind(this);
+		
 	}
 
     componentDidMount() {
-		console.log("API");
-		// Axios.get("http://localhost:3001/users").then(r => {
-		// 	this.setState({list: r.data});
-		// })
-        Axios.get("http://localhost:3001/userinfo", {params: {id: 1}}).then(r => {
-            console.log("r: " + r);
-            this.setState({username: r.username});
-            this.setState({userId: r.id});
-            this.setState({email: r.email});
-            this.setState({bio: r.bio});
-            this.setState({isMaestro: r.isMaestro});
+        Axios.get("http://localhost:3001/userinfo", {params: {id: this.state.userId}}).then(r => {
+            console.log(r);
+            this.setState({username: r.data[0].username});
+            // this.setState({userId: r.data[0].id});
+            this.setState({email: r.data[0].email});
+            if(r.data[0].bio !== null) {
+                this.setState({bio: r.data[0].bio});
+            }
+            this.setState({isMaestro: r.data[0].isMaestro});
             console.log("username: " + this.state.username);
+
+            this.setState({editName: this.state.username});
+            this.setState({editBio: this.state.bio});
         })
 	}
 
-    setEditing(v) {
-        this.setState({editing: v});
-        console.log("Editing? " + this.state.editing);
+    changeUsername(e) {
+		this.setState({editName: e.target.value});
+        console.log("editname: " + this.state.editName);
+	}
+
+    changeBio(e) {
+        this.setState({editBio: e.target.value});
+        console.log(this.state.editBio);
+    }
+
+    submitEdit(e) {
+        e.preventDefault();
+        // Axios.post("http://localhost:3001/editbio", {}, {params: {id: this.state.userId}}, {
+        //     bio: this.state.editBio
+        // })
+        console.log("new name: " + this.state.editName);
+        console.log("new bio: " + this.state.editBio);
+        console.log("id "+this.state.userId);
+
+        var query = JSON.stringify({id: this.state.userId, newbio: this.state.editBio});
+        console.log(query);
+
+        // Axios.post("http://localhost:3001/editbio", {query: {
+        //     id: this.state.userId,
+        //     newbio: this.state.editBio
+        // }})
+        Axios.post("http://localhost:3001/editrecording", {params: query}).then(r => {
+			this.setState({bio: r.data[0].bio})
+	    })
+        // Axios.post("http://localhost:3001/editbio", {newbio: this.state.editBio}, {params: {
+        //     id: this.state.userId
+        // }})
+        // Axios.post("http://localhost:3001/editbio", {
+        //     bio: this.state.editBio
+        // })
+        // Axios.post("http://localhost:3001/editbio", this.state.userId, {
+        //     bio: this.state.editBio
+        // })
+        // Axios.post("http://localhost:3001/editbio", {}, this.state.userId).then((response) => {
+        //     bio: this.state.editBio
+        // })
+        
+        //this.setState({editing: false});
+    }
+
+    cancelEdit(e) {
+        this.setState({editName: this.state.username});
+        this.setState({editBio: this.state.bio});
+        this.setState({editing: false});
     }
 
     render() {
-        // return (
-        //     <div className='schedule'>
-        //         <div className='dark-overlay'>
-        //             <div className='search-inner'>
-        //                 <div className='search-box'>
-        //                     <h1 className='large text-primary'>Profile {this.state.username}</h1>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // )
-
-        return (
-            <div className='schedule'>
-                <div className='dark-overlay'>
-                    <div className='search-inner'>
-                        <div className='search-box'>
-                            <h1 className='large text-primary'>Profile</h1>
-                            <span>
-                                <p className='landing-text'>Username: {this.state.username}</p>
-                            </span>
-                            <span>
-                                <p className='landing-text'>Email: {this.state.email}</p>
-                            </span>
-                            <span>
-                                <p className='landing-text'>Maestro Status: {this.state.isMaestro}</p>
-                            </span>
-                            <span>
-                                <p className='landing-text'>Bio: {this.state.bio}</p>
-                            </span>
-                            <span style={{align: "center"}}>
-                                <button className='btn btn-primary' onClick={() => this.setState({editing: true})}>Edit</button>
-                            </span>
+        if(this.state.editing) {
+            return(
+                <div className='schedule'>
+                    <div className='dark-overlay'>
+                        <div className='search-inner'>
+                            <div className='search-box'>
+                                <h1 className='large text-primary'>Edit Profile</h1>
+                                <br />
+                                <Fragment>
+                                    <form onSubmit={this.submitEdit}>
+                                        <h1 className='medium'>Username:</h1>
+                                        <input className='edit-info-field' value={this.state.editName} onChange={this.changeUsername} />
+                                        <br />
+                                        <h1 className='medium'>Bio:</h1>
+                                        <input className='edit-info-field' value={this.state.editBio} onChange={this.changeBio} />
+                                        <button type='submit' className='btn btn-primary'>Submit</button>
+                                        <button className='btn btn-primary' onClick={this.cancelEdit}>Cancel</button>
+                                    </form>
+                                </Fragment>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div className='schedule'>
+                    <div className='dark-overlay'>
+                        <div className='search-inner'>
+                            <div className='search-box'>
+                                <h1 className='large text-primary'>Profile</h1>
+                                <span>
+                                    <p className='landing-text'>Username: {this.state.username}</p>
+                                </span>
+                                <span>
+                                    <p className='landing-text'>Email: {this.state.email}</p>
+                                </span>
+                                <span>
+                                    <p className='landing-text'>Maestro Status: {this.state.isMaestro}</p>
+                                </span>
+                                <span>
+                                    <p className='landing-text'>Bio: {this.state.bio}</p>
+                                </span>
+                                <span style={{align: "center"}}>
+                                    <button className='btn btn-primary' onClick={() => this.setState({editing: true})}>Edit</button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+        
     }
 }
-
-// const Profile = () => {
-//     // const [userId, setId] = useState(0);
-//     // const [userName, setuserName] = useState('');
-//     // const [email, setemail] = useState('');
-//     // const [isMaestro, setisMaestro] = useState(0);
-//     // const [bio, setbio] = useState('');
-//     // const [token, setToken] = useState('');
-//     // const [expire, setExpire] = useState('');
-
-//     // const [editing, setediting] = useState(false);
-
-//     useEffect(() =>
-//     {
-//         refreshToken();
-//     },[]);
-
-//     const refreshToken = async () => {
-//         try {
-//             const response = await axios.get('http://localhost:3001/token');
-//             setToken(response.data.accessToken);
-//             const decoded = jwt_decode(response.data.accessToken);
-//             console.log("decoded id", decoded.userId);
-//             setId(decoded.userId);
-//             setuserName(decoded.username);
-//             setemail(decoded.email);
-//             setisMaestro(decoded.isMaestro);
-//             setbio(decoded.bio);
-//             console.log("username after decoded", userName);
-//             console.log("userid after decoded", userId);
-//             setExpire(decoded.exp);
-//             console.log("heres token", decoded);
-//             console.log("HERE: " + isMaestro);
-//             setAuthToken(decoded);
-//         } catch (error) {
-//             if (error.response) {
-//                // history.push("/");
-//                console.log("auth fail");
-//             }
-//         }
-//     }
-
-//     const axiosJWT = axios.create();
-
-//     axiosJWT.interceptors.request.use(async (config) => {
-//         const currentDate = new Date();
-//         if (expire * 1000 < currentDate.getTime()) {
-//             const response = await axios.get('http://localhost:3001/token');
-//             config.headers.Authorization = `Bearer ${response.data.accessToken}`;
-//             setToken(response.data.accessToken);
-//             const decoded = jwt_decode(response.data.accessToken);
-//             setuserName(decoded.username);
-//             setExpire(decoded.exp);
-
-//             // getRecordings(decoded.userId);
-//             // getEvents(decoded.userId);
-//             console.log("username after decoded", userName);
-//             console.log("userid after decoded", userId);
-//             console.log("isMaestro: " + isMaestro);
-//         }
-//         return config;
-//     }, (error) => {
-//         return Promise.reject(error);
-//     });
-
-//     return (
-//         <div className='schedule'>
-//             <div className='dark-overlay'>
-//                 <div className='search-inner'>
-//                     <div className='search-box'>
-//                         <h1 className='large text-primary'>Profile</h1>
-//                         <span>
-//                             <p className='landing-text'>Username: {userName}</p>
-//                         </span>
-//                         <span>
-//                             <p className='landing-text'>Email: {email}</p>
-//                         </span>
-//                         <span>
-//                             <p className='landing-text'>Maestro Status: {isMaestro}</p>
-//                         </span>
-//                         <span>
-//                             <p className='landing-text'>Bio: {bio}</p>
-//                         </span>
-//                         <span style={{align: "center"}}>
-//                             <button className='btn btn-primary' onClick={setediting(true)}>Edit</button>
-//                         </span>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
 
 export default Profile;
